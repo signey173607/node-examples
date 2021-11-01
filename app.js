677f7eb52d7973b248c1a0bafc0fb1db9eaa2537
admin <admin@admins-MacBook-Pro.local>
2021-10-31 22:46:37 -0400

    node exports test

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const rect = require('./rectangle');
+const rect = require('./rectangle.js');
 
-function solveRect(l,w) {
-    console.log(`Solving for rectangle with values of: ${l}, ${w}`);
+function solveRectangle(l, w) {
+    console.log(`Solving for rectangle with values of ${l}, ${w}.`);
 
-    if(l <= 0 || w <=0 ) {
-        console.log(`Rectangle dimensions must be greater than zero. Recieved: ${l}, ${w}`);
+    if(l<=0 || w<=0) {
+        console.log(`The values of rectangle should be greater than zero. given: ${l}, ${w}.`);
     }
-    else {
-        console.log(`Area of rectangle is: ${rect.area(l,w)}`);
-        console.log(`Perimeter of the rectangle is ${rect.perimeter(l,w)}`);
+
+    else{
+        console.log(`The area of rectangle is: ${rect.area(l,w)}.`);
+        console.log(`The perimeter of rectangle is: ${rect.perimeter(l,w)}.`);
     }
 }
 
-solveRect(3,5);
-solveRect(10,5);
-solveRect(0, 5);
-solveRect(4, -4);
+solveRectangle(4,6);
+solveRectangle(0,7);
+solveRectangle(-2,8);
+solveRectangle(9,5);
